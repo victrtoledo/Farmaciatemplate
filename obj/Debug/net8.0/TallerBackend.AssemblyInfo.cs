0cb95b68fb9b292a7fab7b5951c2940b18e69fb0
victor <1918967@alu.murciaeduca.es>
2026-05-03 19:46:17 +0200

    cambios visuales y de seo en todas las secciones

--- obj/Debug/net8.0/TallerBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/TallerBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TallerBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70f8a497beaa06b24dbfec1bfbf5e8bffcf33a40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6409b988b1b8f40a8bdfa6681accbd52625702b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TallerBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TallerBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
